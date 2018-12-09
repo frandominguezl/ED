40200a05201dd7c9b0215063abd0418ab578b0f6
frandominguez03 <d3vcho@gmail.com>
2018-12-09 17:47:49 +0100

    Eliminar HTML innecesario

--- Practica 4 - Diccionario STL/doc/html/annotated_dup.js
+++
@@ -1,5 +0,0 @@
-var annotated_dup =
-[
-    [ "Diccionario", "classDiccionario.html", "classDiccionario" ],
-    [ "Termino", "classTermino.html", "classTermino" ]
-];
--- Practica 4 - Diccionario STL/doc/html/dir_d44c64559bbebec7f509842c48db8b23.js
+++
@@ -1,5 +0,0 @@
-var dir_d44c64559bbebec7f509842c48db8b23 =
-[
-    [ "Diccionario.h", "Diccionario_8h_source.html", null ],
-    [ "Termino.h", "Termino_8h_source.html", null ]
-];
